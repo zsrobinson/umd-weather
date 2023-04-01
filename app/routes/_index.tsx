06c7ee663763d1211e5f6032ab1233a2600d74dd
Zachary Robinson <me@zsrobinson.com>
2023-04-01 12:21:58 -0400

    extract fetch to getWeather function

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,11 +2,36 @@ import type { LoaderArgs, V2_MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: "UMD Weather" }];
 };
 
 export async function loader({}: LoaderArgs) {
-  const data = await fetch(
+  const weather = await getWeather(5, "min");
+  return { weather };
+}
+
+export default function Index() {
+  let { weather } = useLoaderData<typeof loader>();
+
+  weather = weather.data.map((obj: any) => obj.dateTime);
+
+  return (
+    <div>
+      <h1>Welcome to UMD Weather</h1>
+      <pre>{JSON.stringify(weather, null, 2)}</pre>
+    </div>
+  );
+}
+
+/**
+ * Gets the weather data from the latest x minutes/hours
+ * @param duration number of units to get data for
+ * @param units either minutes ("min") or hours ("hr")
+ */
+async function getWeather(duration: number, units: "min" | "hr") {
+  const min = units === "min" ? duration : duration * 60;
+
+  const req = await fetch(
     "https://weather.umd.edu/wordpress/wp-content/plugins/meso-fsct/functions/get-data.php",
     {
       method: "POST",
@@ -14,7 +39,7 @@ export async function loader({}: LoaderArgs) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        startms: new Date(Date.now() - 1000 * 60 * 5).getTime() / 1000,
+        startms: new Date(Date.now() - 1000 * 60 * min).getTime() / 1000,
         endms: Date.now() / 1000,
         db: "mesoterp7DB",
         cols: [
@@ -29,18 +54,6 @@ export async function loader({}: LoaderArgs) {
         ],
       }),
     }
-  ).then((res) => res.json());
-
-  return { data };
-}
-
-export default function Index() {
-  const { data } = useLoaderData<typeof loader>();
-
-  return (
-    <div>
-      <h1>Welcome to UMD Weather</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
   );
+  return await req.json();
 }
