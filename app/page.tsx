fdb057e3b2200058297d9cbcb44ceaaac9a5e1ad
Zachary Robinson <me@zsrobinson.com>
2023-04-06 16:11:06 -0400

    add debug and github icons

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,47 @@ import { FeelsLikeWidget } from "@/components/FeelsLikeWidget";
 import { TempWidget } from "@/components/TempWidget";
 import { UpdatedAt } from "@/components/UpdatedAt";
 import { OneByOne, OneByTwo, TwoByTwo } from "@/components/base-widgets";
+import { IconBrandGithub, IconBug } from "@tabler/icons-react";
+import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="h-screens mx-auto flex w-fit flex-col items-center gap-8 p-8">
-      <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-semibold">UMD Weather</h1>
-        <UpdatedAt />
-      </div>
-      <div className="grid grid-cols-2 grid-rows-3 gap-6 lg:grid-cols-4">
-        <TempWidget />
-        <FeelsLikeWidget />
-        <TwoByTwo className="whitespace-pre">Hello World!</TwoByTwo>
-        <OneByTwo>Bonjour</OneByTwo>
-        <OneByOne>Hello</OneByOne>
-        <OneByOne>World</OneByOne>
-        <OneByTwo>Bonjour</OneByTwo>
-      </div>
-    </main>
+    <>
+      <main className="mx-auto flex h-screen w-fit flex-col items-center gap-8 p-8">
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl font-semibold">UMD Weather</h1>
+          <UpdatedAt />
+        </div>
+        <div className="grid grid-cols-2 grid-rows-3 gap-6 lg:grid-cols-4">
+          <TempWidget />
+          <FeelsLikeWidget />
+          <TwoByTwo className="whitespace-pre">Hello World!</TwoByTwo>
+          <OneByTwo>Bonjour</OneByTwo>
+          <OneByOne>Hello</OneByOne>
+          <OneByOne>World</OneByOne>
+          <OneByTwo>Bonjour</OneByTwo>
+        </div>
+      </main>
+      <FooterButtons />
+    </>
+  );
+}
+
+export function FooterButtons() {
+  return (
+    <div className="absolute bottom-4 right-4 flex gap-2">
+      <Link
+        href={"/debug"}
+        className="rounded-full bg-gray-700 bg-opacity-50 p-2 text-gray-500 transition hover:bg-opacity-70"
+      >
+        <IconBug />
+      </Link>
+      <Link
+        href={"https://www.github.com/zsrobinson/umd-weather"}
+        className="rounded-full bg-gray-700 bg-opacity-50 p-2 text-gray-500 transition hover:bg-opacity-70"
+      >
+        <IconBrandGithub />
+      </Link>
+    </div>
   );
 }
