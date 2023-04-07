bbc40273fcac76c2e508c861a4fa84a596d9d2c4
Zachary Robinson <me@zsrobinson.com>
2023-04-07 16:00:39 -0400

    escape apostrophes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,28 +18,28 @@ export default function Page() {
       <div className="flex gap-4">
         <div className="flex max-w-2xl flex-col gap-4 rounded-3xl bg-gray-700 bg-opacity-50 p-6 text-gray-300">
           <p>
-            Hey there, my name's Zach! I was inspired by the Mesoterps project
-            at the University of Maryland to create a little weather website. I
-            was originally planning on integrating data from Mesoterps into this
-            website, though it only returns current and historical data instead
-            of forecasting, something that would be more useful for a weather
-            app. Instead, I'm using the One Call API from OpenWeatherMap to
-            retrieve all of this data for the site.
+            Hey there, my name&apos;s Zach! I was inspired by the Mesoterps
+            project at the University of Maryland to create a little weather
+            website. I was originally planning on integrating data from
+            Mesoterps into this website, though it only returns current and
+            historical data instead of forecasting, something that would be more
+            useful for a weather app. Instead, I&apos;m using the One Call API
+            from OpenWeatherMap to retrieve all of this data for the site.
           </p>
           <p>
-            For some of the technical details, I'm using the Next.js Beta App
-            Directory and React Server Components to render everything. Most of
-            my full stack web dev experience has been with Remix, so I wanted to
-            try out something new. This is all hosted on Vercel and the data is
-            revalidated every two minutes. Since I only get 1,000 free API calls
-            per day from OpenWeatherMap, this ensures I don't go over that
-            limit.
+            For some of the technical details, I&apos;m using the Next.js Beta
+            App Directory and React Server Components to render everything. Most
+            of my full stack web dev experience has been with Remix, so I wanted
+            to try out something new. This is all hosted on Vercel and the data
+            is revalidated every two minutes. Since I only get 1,000 free API
+            calls per day from OpenWeatherMap, this ensures I don&apos;t go over
+            that limit.
           </p>
           <p>
             Feel free to star the project or dig into the source code on GitHub!
-            If there's a bug or a feature you'd like to see, feel free to reach
-            out to me about it, open an issue on GitHub, or even better, submit
-            a pull request!
+            If there&apos;s a bug or a feature you&apos;d like to see, feel free
+            to reach out to me about it, open an issue on GitHub, or even
+            better, submit a pull request!
           </p>
         </div>
         <div className="flex flex-col gap-4">
